682d832c4d80a444f4023e7bdc660197e9cf41eb
Shangqing Song (Steven) <66652771+stevensong077@users.noreply.github.com>
2020-11-11 19:25:20 +1100

    add jsx

---
+++ src/container/Orders.jsx
@@ -0,0 +1,21 @@
+// import React, { useState, useEffect } from 'react';
+
+// function Orders(props) {
+//     const { data } = props;
+//     const [filterText, setFilterText] = useState('');
+//     return ( <OrderTable tableData={data}/>)
+// }
+
+// Orders.defaultProps = {
+//     data: [
+//         {
+//             orderNumber: '1234',
+//             customerId: 'roiwef',
+//             orderItems: [{
+//             }]
+//         }
+//     ]
+// };
+
+// export default Orders;
+
